9815a4a758f5b0e209c8c79642c3500ccb438482
Jay Malhotra <jay0@me.com>
2025-03-02 18:55:02 +0000

    Updates

--- Makefile
+++ Makefile
@@ -1,25 +1,24 @@
 CXX=g++
-CXXFLAGS=-mavx2 -g -pg -O3 -std=c++23 -D SIMD
+CXXFLAGS=-mavx2 -O3 -std=c++23 -D SIMD
 TARGET=bin/main
 ASMFILE=bin/main.s
 SRCFILE=main.cpp
+
+.PHONY: profile run-profile clean
 
 all: $(TARGET)
 
 $(TARGET): $(SRCFILE)
 	$(CXX) $(CXXFLAGS) $(SRCFILE) -o $(TARGET)
 
-asm: $(SRCFILE)
+$(ASMFILE): $(SRCFILE)
 	$(CXX) $(CXXFLAGS) -S $(SRCFILE) -o $(ASMFILE) 
 
-.PHONY: profile
 profile: all run-profile
 
-.PHONY: run-profile
 run-profile:
 	rm -f callgrind.out.*
 	valgrind --tool=callgrind --callgrind-out-file=./bin/callgrind.out $(TARGET) 
 
-.PHONY: clean
 clean:
 	rm -f $(TARGET) $(ASMFILE)
--- main.cpp
+++ main.cpp
@@ -10,20 +10,24 @@
 
 constexpr size_t num_cols = 7;
 
-template <size_t NCols> class CsvParseResult {
+template <size_t NCols>
+class CsvParseResult
+{
 public:
   typedef std::array<std::string, NCols> Row;
 
   CsvParseResult(Row column_names, std::vector<Row> rows)
       : column_names(std::move(column_names)), rows(std::move(rows)) {}
 
-  std::unordered_map<std::string, std::string> get_row(int row_num) const {
+  std::unordered_map<std::string, std::string> get_row(int row_num) const
+  {
     std::unordered_map<std::string, std::string> result;
     result.reserve(NCols);
 
     auto row = rows.at(row_num);
 
-    for (int i = 0; i < NCols; i++) {
+    for (int i = 0; i < NCols; i++)
+    {
       result.emplace(column_names[i], row[i]);
     }
 
@@ -38,30 +42,36 @@ private:
 constexpr int CHUNK_SIZE = 256 / 8; // how many chars we can fit in one vector
 
 // Function to display SIMD array data
-void display_simd_array(const char *message, __m256i simdArray) {
+void display_simd_array(const char *message, __m256i simdArray)
+{
   char *data = (char *)&simdArray;
   std::cout << message;
-  for (int i = 0; i < CHUNK_SIZE; ++i) {
+  for (int i = 0; i < CHUNK_SIZE; ++i)
+  {
     std::cout << data[i] << ", ";
   }
   std::cout << std::endl;
 }
 
-void display_simd_array_numeric(const char *message, __m256i simdArray) {
+void display_simd_array_numeric(const char *message, __m256i simdArray)
+{
   uint8_t *data = (uint8_t *)&simdArray;
   std::cout << message;
-  for (int i = 0; i < CHUNK_SIZE; ++i) {
+  for (int i = 0; i < CHUNK_SIZE; ++i)
+  {
     std::cout << static_cast<int>(data[i]) << ", ";
   }
   std::cout << std::endl;
 }
 
 void read_col_names(const std::string &row_str,
-                    CsvParseResult<num_cols>::Row *output) {
+                    CsvParseResult<num_cols>::Row *output)
+{
   size_t pos = 0;
   size_t last = 0;
   size_t idx = 0;
-  while ((pos = row_str.find(',', last)) != std::string::npos) {
+  while ((pos = row_str.find(',', last)) != std::string::npos)
+  {
     auto col_name = row_str.substr(last, pos - last);
     output->at(idx) = col_name;
     last = pos + 1;
@@ -72,20 +82,21 @@ void read_col_names(const std::string &row_str,
 }
 
 void read_row(const std::string &row_str,
-              CsvParseResult<num_cols>::Row *output) {
+              CsvParseResult<num_cols>::Row *output)
+{
 
   bool open_quote = false;
   int processed = 0;
 
   int col_start = 0;
   int col_num = 0;
-  int backtrack = 0;
 
+#ifdef SIMD // SIMD does not actually make the program observably faster...
   __m256i comma = _mm256_set1_epi8(',');
   __m256i quote = _mm256_set1_epi8('"');
 
-#ifdef SIMD
-  while (processed < static_cast<int>(row_str.length()) - CHUNK_SIZE) {
+  while (processed < static_cast<int>(row_str.length()) - CHUNK_SIZE)
+  {
     const char *ptr = row_str.c_str() + processed;
 
     __m256i chunk = _mm256_loadu_si256((__m256i_u *)ptr);
@@ -99,14 +110,18 @@ void read_row(const std::string &row_str,
     auto comma_int_mask = _mm256_movemask_epi8(comma_eq);
     auto quote_int_mask = _mm256_movemask_epi8(quote_eq);
 
-    while (quote_int_mask || comma_int_mask) {
+    while (quote_int_mask || comma_int_mask)
+    {
       uint32_t quote_pos = __builtin_ctz(quote_int_mask);
       uint32_t comma_pos = __builtin_ctz(comma_int_mask);
 
-      if (quote_pos < comma_pos) {
+      if (quote_pos < comma_pos)
+      {
         open_quote = !open_quote;
         quote_int_mask &= quote_int_mask - 1; // Set least significant bit to 0
-      } else if (!open_quote) {
+      }
+      else if (!open_quote)
+      {
         auto current_string_pos = processed + comma_pos;
         auto col_value =
             row_str.substr(col_start, current_string_pos - col_start);
@@ -116,7 +131,9 @@ void read_row(const std::string &row_str,
         col_num++;
 
         comma_int_mask &= comma_int_mask - 1;
-      } else {
+      }
+      else
+      {
         break;
       }
     }
@@ -126,12 +143,16 @@ void read_row(const std::string &row_str,
 #endif
 
   // Scalar fallback
-  for (int i = processed + 1; i < row_str.size(); i++) {
+  for (int i = processed + 1; i < row_str.size(); i++)
+  {
     char ch = row_str[i];
 
-    if (ch == '"') {
+    if (ch == '"')
+    {
       open_quote = !open_quote;
-    } else if (ch == ',' && !open_quote) {
+    }
+    else if (ch == ',' && !open_quote)
+    {
       auto col_value = row_str.substr(col_start, i - col_start);
       output->at(col_num) = col_value;
 
@@ -144,7 +165,8 @@ void read_row(const std::string &row_str,
       row_str.substr(col_start, row_str.size() - 1 - col_start);
 }
 
-int main() {
+int main()
+{
 
 #ifdef SIMD
   std::cout << "Using SIMD for parsing" << std::endl;
@@ -156,7 +178,8 @@ int main() {
   std::ifstream input_file("annual-enterprise-survey-2023-financial-year-"
                            "provisional-size-bands.csv");
 
-  if (!input_file.is_open()) {
+  if (!input_file.is_open())
+  {
     std::cout << "Failed to open file" << std::endl;
     return 1;
   }
@@ -171,7 +194,8 @@ int main() {
   read_col_names(line, &column_names);
   CsvParseResult<num_cols>::Row row;
 
-  while (std::getline(input_file, line)) {
+  while (std::getline(input_file, line))
+  {
     read_row(line, &row);
     rows.push_back(row);
   }
@@ -180,11 +204,10 @@ int main() {
 
   auto end = std::chrono::high_resolution_clock::now();
 
-  for (auto pair : result.get_row(1)) {
+  for (auto pair : result.get_row(1))
+  {
     std::cout << pair.first << ": " << pair.second << "\n";
   }
-
-  auto t2 = std::chrono::high_resolution_clock::now();
 
   std::chrono::duration<float> elapsed_ms =
       std::chrono::duration_cast<std::chrono::duration<float>>(end - start);
